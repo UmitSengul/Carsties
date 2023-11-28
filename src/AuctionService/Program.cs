a62b067b9828d1c8d2f5c07a5fef263104f1d8be
Ümit Sengül <umitsengul.de@gmail.com>
2023-11-28 12:55:16 +0300

    Database Created in Docker and Migrated

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,7 +1,12 @@
+using AuctionService;
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 
@@ -10,5 +15,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 
-
+try
+{
+    DbInitializer.Initdb(app);
+}
+catch (Exception e)
+{
+    
+Console.WriteLine(e);
+}
 app.Run();
