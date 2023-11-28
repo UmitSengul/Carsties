3aec56f5bed70137407804fc3e86070b0b0ee50c
Ümit Sengül <umitsengul.de@gmail.com>
2023-11-28 19:29:09 +0300

    26 ders sonu. mongodb baglandı.

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,30 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+
+app.UseAuthorization();
+app.UseAuthentication();
+app.MapControllers();
+
+await DB.InitAsync("SearchDb",MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+
+await DB.Index<Item>()
+    .Key(x=>x.Make,KeyType.Text)
+    .Key(x=>x.Model,KeyType.Text)
+    .Key(x=>x.Color,KeyType.Text)
+    .CreateAsync();
+
+
+app.Run();
