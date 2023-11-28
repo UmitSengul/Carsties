f10f393c0eb0f0d62842a842382c4d07dd111ea8
Ümit Sengül <umitsengul.de@gmail.com>
2023-11-28 13:43:54 +0300

    Ders 15 Sonu

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -6,14 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AuctionDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseAuthorization();
+
+app.MapControllers();
 
 try
 {
@@ -21,7 +26,7 @@ try
 }
 catch (Exception e)
 {
-    
-Console.WriteLine(e);
+    Console.WriteLine(e);
 }
+
 app.Run();
