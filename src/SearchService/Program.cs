db52d17a0694eabcc9a35de19cacdd90fe5c09e6
Ümit Sengül <umitsengul.de@gmail.com>
2023-12-11 14:24:29 +0300

    37 ders sonu

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -12,6 +13,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetRetryPolicy());
+builder.Services.AddMassTransit(
+    x =>
+    {
+        x.UsingRabbitMq(
+            (context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            }
+        );
+    }
+);
 
 var app = builder.Build();
 
