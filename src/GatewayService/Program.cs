7b3df6b16ef6d79433ffd1ae9d9fda4e76bf1a40
Ümit Sengül <umitsengul.de@gmail.com>
2023-12-13 20:59:15 +0300

    68 ders sonu

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
 
 
 
@@ -12,5 +21,7 @@ builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSecti
 var app = builder.Build();
 
 app.MapReverseProxy();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
