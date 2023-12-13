95d4079018602c9913ef27f675631047f7052a61
Ümit Sengül <umitsengul.de@gmail.com>
2023-12-13 20:15:10 +0300

    64 ders sonu

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+
+
+
+
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.Run();
