9768df29385a3bafb85d87d75309b2082fd89e40
Ümit Sengül <umitsengul.de@gmail.com>
2024-01-16 13:58:48 +0300

    Bölüm 8 sonu. paketler ve yazım güncellendi

--- frontend/web-app/next.config.js
+++ frontend/web-app/next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // experimental: {
+    //   serverActions: true
+    // },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.pixabay.com',
+          pathname: '**',
+        },
+      ],
+    },
+    webpack: (config, { dev, isServer }) => {
+      // Add the necessary Babel plugins and presets here
+      if (!isServer) {
+        config.resolve.alias['next/babel'] = require.resolve('@babel/core');
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
